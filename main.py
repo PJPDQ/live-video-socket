a29a7c6833a1ee5590c74f8f2e96cc682f029175
Dicky Sentosa Gozali <n9502271@qut.edu.au>
2024-02-08 17:30:15 +1000

    Storing the live video to image frame.

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import sys
+import os
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO
 import cv2
 import base64
+import time
 app = Flask(__name__)
 sio = SocketIO(app)
+
+cwd = os.getcwd()
+save_dir = f'{cwd}\\saved_dir'
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
 
 @app.route('/')
 def index():
@@ -17,7 +24,7 @@ def message(json, methods=['GET','POST']):
     '''
     Sends message into frame-by-frame into .jpg format through websocket.
     '''
-    print("Recieved message")
+    # print("Recieved message")
     sio.emit('image', json)
       
 @sio.on('live_feed')
@@ -25,14 +32,23 @@ def live_feed(json):
     '''
     Send live feed from opencv video webcam to bytes jpg.
     '''
-    cap=cv2.VideoCapture(0)	
+    os.chdir(save_dir)
+    cap=cv2.VideoCapture(0)
+    digit = len(str(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))))
+    n = 0
     while(cap.isOpened()):
         ret,img=cap.read()
         if ret:
+            img_file = f"sample_{str(n).zfill(digit)}.jpg"
             img = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
+            is_write = cv2.imwrite(img_file, img)
+            # for debugging purpose to see if the image is saved and stored.
+            # if is_write:
+            #     print(f"image saved and can be found in {img_file}")
             frame = cv2.imencode('.jpg', img)[1].tobytes()
             frame= base64.encodebytes(frame).decode("utf-8")
             message(frame)
+            n+=1
             sio.sleep(0)
         else:
             break
